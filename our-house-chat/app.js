b7396d43c76599abef09b792e0ff98b76dc9a596
Staci_Harding <harding8@pdx.edu>
2021-02-06 19:42:43 -0800

    logging added, parsing added

--- our-house-chat/app.js
+++ our-house-chat/app.js
@@ -1,22 +1,48 @@
 //uses express application
 
-const express = require('express'); //package added via npmf
-
+const express = require('express'); // package added via npm
 const app = express(); // can use utility methods, functionality etc
+const morgan = require('morgan'); // logging
+const bodyParser = require('body-parser'); // parse requests - url encoded or json
 
 const userRoutes = require('./api/routes/users');
 const roomRoutes = require('./api/routes/rooms');
 
+app.use(morgan('dev'));
+// which bodies are we parsing, javascript object confirguration simple bodies
+app.use(bodyParser.urlencoded({extended: false}));
+
 // middleware!
+// routes that will handle requests
 // route, route handler
 app.use('/users', userRoutes);
 app.use('/rooms', roomRoutes);
 
+// error handling, no routes caught request 
+// control has reached function
+app.use((req, res, next)=> {
+    // create error object
+    const error = new Error('Not Found');
+    error.status = 404;
+    // pass error along to next handle
+    next(error);
+});
+// 600 errors will reach this error
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
+//// for initial port test
+//// request, response, execute next() (move request to next middleware in line)
 // app.use((req, res, next) => {
-//     // for initial port test
-//     // request, response, execute next() (move request to next middleware in line)
+//     
 //     // 200 = everything ok
-//     // javascript object in string format
+//     // javascript object always in string format
 //     // res.status(200).json({
 //     //     message: 'It works!'
 //     // });
--- our-house-chat/server.js
+++ our-house-chat/server.js
@@ -3,8 +3,11 @@
 const http = require('http'); //provides functionality for spinning up a sever
 const app = require('./app'); //automatically looking or js files
 
-const port = process.env.PORT || 3000; // would be set on server I provide the product on, else default
+// would be set on server I provide the product on, else default
+const port = process.env.PORT || 3000; 
 
-const server = http.createServer(app); //need to pass a listener to execute funtion for each new request
+//need to pass a listener to execute funtion for each new request
+const server = http.createServer(app); 
 
-server.listen(port); //starts listening on this port, start executing whatever request is received via listener.
+//starts listening on this port, start executing whatever request is received via listener.
+server.listen(port); 
